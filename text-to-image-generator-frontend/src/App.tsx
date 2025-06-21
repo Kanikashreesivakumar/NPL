579b22963e409cfd11f916d3f18c914d2f50dcc3
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-06-21 11:47:28 +0530

    changes

--- text-to-image-generator-frontend/src/App.tsx
+++ text-to-image-generator-frontend/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
+import ImageGenerator from './components/imagegenerator';
+import Navbar from './components/navbar';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="container mx-auto px-4 py-8">
+        <ImageGenerator />
+      </main>
     </div>
   );
 }
---
+++ text-to-image-generator-frontend/src/components/imagegenerator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageGenerator: React.FC = () => {
+  return (
+    <div>
+     
+      <p>Image Generator Component</p>
+    </div>
+  );
+};
+
+export default ImageGenerator;
